998e26b7af4e61248e4fcabfa16b3c50a2a90fdd
michoel kahana <kmichoel25@gmail.com>
2023-02-14 16:29:48 -0500

    Added MyProfile Btn, and Profile files and components

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 import "./Header.css"
 import Search from "./Search"
+import MyProfile from './MyProfile'
 import {Link} from 'react-router-dom'
 
-
 export default function Header({search, setSearch}) {
-
   return (
     <div className="Header">
       <Link to='/'><button>Home</button></Link>
       <Search search={search}  setSearch={setSearch}/>
       <Link to='/collections'><button>Go to my Collection</button></Link>
       <Link to='/signup'><button className="signupBtn">Sign Up</button></Link>
-
+      <Link to='/profile'><button className='profileBtn'>My Profile:</button></Link>
+      {/* <MyProfile/> */}
     </div>
   )
-}
+} 
