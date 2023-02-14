1e6df39f4dde781035e11d9e6780ac40ceb5d6e9
Shadiya Egal <shadiya325@gmail.com>
2023-02-13 20:19:18 -0500

    search bar and collection

---
+++ frontend/src/components/Collections.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Collections.css'
+
+function Collections({books}) {
+
+    // const handleClick = () => {
+    //     console.log("hi")
+    // }
+
+
+    return (
+        <div className="collection" 
+        // onClick={handleClick}
+        > Collection 
+        </div>
+    )
+}
+
+export default Collections;
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Search.css'
+
+function Search() {
+    return (
+        <div className="searchBar">
+        <input className='search-name'/>
+        <button className='searchButton'>Search</button>
+        </div>
+        )
+}
+
+export default Search;
