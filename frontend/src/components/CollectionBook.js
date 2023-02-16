57cf116e587e59a40f34ec4cfdfaa8abf3ba695a
Klajdo Qasolli <63808442+KlajdoQ@users.noreply.github.com>
2023-02-16 10:11:18 -0500

    patch almost done

--- frontend/src/components/CollectionBook.js
+++ frontend/src/components/CollectionBook.js
@@ -1,74 +1,81 @@
-import React, {useState, useEffect} from 'react'
-import './CollectionBook.css'
-import NotesList from './NotesList'
+import React, { useState} from "react";
+import "./CollectionBook.css";
+import NotesList from "./NotesList";
 
-export default function CollectionBook({myBooks,setToggle}) {
-    const {title, image_url, author, published_year, description,id} = myBooks
-    const [noteText, setNoteText] = useState('')
-    const [noteArr, setNoteArr] = useState([])
+export default function CollectionBook({ myBooks, setToggle }) {
+  const { title, image_url, author, published_year, description, id } = myBooks;
+  const [noteText, setNoteText] = useState("");
+  const [noteArr, setNoteArr] = useState([]);
 
- // Delete a book from the collection 
+  // Delete a book from the collection
   function removeBook(id) {
-      fetch(`http://localhost:9292/collections/${id}`, {
-        method: 'DELETE',
-      })
-        .then(response => response.json())
-        .then(data => setToggle(prev=>!prev))
-        .catch(error => console.error('Error:', error));
-    }
+    fetch(`http://localhost:9292/collections/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((data) => setToggle((prev) => !prev))
+      .catch((error) => console.error("Error:", error));
+  }
 
- //Post a note to the book 
+  //Post a note to the book
   function handleChange(e) {
-    setNoteText(e.target.value)
+    setNoteText(e.target.value);
   }
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
     fetch(`http://localhost:9292/collections/${id}/notes`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         note: noteText,
         reader_id: 9,
-        book_id: id
+        book_id: id,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setNoteArr((prevNotes) => [...prevNotes, data]);
+        setNoteText("");
       })
-    })
-    .then(resp => resp.json())
-    .then(data => {
-      setNoteArr(prevNotes => [...prevNotes, data])
-      setNoteText('')
-    })
-    .catch(error => console.error('Error:', error))
-}
+      .catch((error) => console.error("Error:", error));
+  }
+
+ // Update /Patch a note 
+  function handleUpdateNote(updatedNote) {
+    const noteIndex = noteArr.findIndex((note) => note.id === updatedNote.id);
+    const updatedNotes = [...noteArr];
+    updatedNotes[noteIndex] = updatedNote;
+    setNoteArr(updatedNotes);
+  }
 
   return (
     <div className="myBooks-div">
-       <img src={image_url} alt="book-image"/>
-         <div className="desc-div">
-          <h3>{title}</h3>
-          <h4>{author}</h4>
-          <h3>Published : {published_year}</h3>
-          <p>{description}</p>
-          <button onClick={(e)=>removeBook(id)}>Remove from collection</button>
-          <form onSubmit={handleSubmit}>
-            <button type="submit">Add Notes</button>
-            <textarea
-              type="text" 
-              name="noteText" 
-              value={noteText} 
-              onChange={handleChange} 
-            ></textarea>
-          
-          </form >
-          {noteArr.map((note, index) => {
-            
-            console.log(note)
-            return <NotesList 
-            key={index} 
-            note={note.note} />
-            })}
-                       
-         </div>
+      <img src={image_url} alt="book-image" />
+      <div className="desc-div">
+        <h3>{title}</h3>
+        <h4>{author}</h4>
+        <h3>Published : {published_year}</h3>
+        <p>{description}</p>
+        <button onClick={(e) => removeBook(id)}>Remove from collection</button>
+        <form onSubmit={handleSubmit}>
+          <button type="submit">Add Notes</button>
+          <textarea
+            type="text"
+            name="noteText"
+            value={noteText}
+            onChange={handleChange}
+          ></textarea>
+        </form>
+        {noteArr.map((note, index) => (
+          <NotesList
+          key={index}
+          note={note}
+          bookId={id}
+          onUpdateNote={handleUpdateNote}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
